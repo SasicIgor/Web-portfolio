1659abfcb07e7fbf60febde0b03897e71befa357
Igor-Sasic <igor.sasic811@gmail.com>
2025-11-20 17:41:26 +0100

    images

--- modules/slider/data.js
+++ modules/slider/data.js
@@ -3,7 +3,7 @@ export const data = [
     title: "Dice game",
     description:
       "This is one of the projects that i did while i was finishing my front-end certificate. The acent of this project was to learn MVC pattern and how to store informations within your code.",
-    imageUrl: "./assets/Dice-game.png",
+    imageUrl: "./assets/Dice-game.jpg",
     projectLink: "https://sasicigor.github.io/Dice-game/",
     codeLink: "https://github.com/SasicIgor/Dice-game",
   },
@@ -11,14 +11,14 @@ export const data = [
     title: "Quiz game",
     description:
       "Goal of this game was to get familiar with TypeScript and Context API.",
-    imageUrl: "./assets/Quiz-game.png",
+    imageUrl: "./assets/Quiz-game.jpg",
     projectLink: "https://sasicigor.github.io/Quiz-app/",
     codeLink: "https://github.com/SasicIgor/Quiz-app",
   },
   {
     title: "Message me (backend)",
     description: "This is backend project in node/express as start of the chat app project.",
-    imageUrl: "./assets/Mascots-project.png",
+    imageUrl: "./assets/Server.jpg",
     projectLink: "",
     codeLink: "https://github.com/SasicIgor/Message-me-backend",
   },
@@ -26,7 +26,7 @@ export const data = [
     title: "Mascots project",
     description:
       "Project was developed in collaboration with a colleague with the goal of automating the mascot booking process for events. It is a full-stack application built with the MERN stack and TypeScript for type safety. We used TanStack Query for efficient data fetching and caching, and employed React Context for state management across the application.",
-    imageUrl: "./assets/Mascots-project.png",
+    imageUrl: "./assets/Mascots-project.jpg",
     projectLink: "",
     codeLink: "https://github.com/SasicIgor/mascots",
   },
@@ -34,7 +34,7 @@ export const data = [
     title: "Next.js Store Project",
     description:
       "This project is the first project I did with using Next. Working on this project I used Prisma for communication with DB (Supabase for images/MongoDB for informations), clerk for authorisation and zod for validations.",
-    imageUrl: "./assets/Nextjs-store.png",
+    imageUrl: "./assets/Nextjs-store.jpg",
     projectLink:
       "https://next-js-store-project-igors-projects-349ff442.vercel.app/",
     codeLink: "https://github.com/SasicIgor/NEXTjs-store-project",
